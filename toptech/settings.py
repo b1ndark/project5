a9b07cc0015908faf64744c06c0b4ecb39bceea4
b1ndark <>
2024-01-16 21:31:45 +0000

    add home app, index.html, views and urls

--- toptech/settings.py
+++ toptech/settings.py
@@ -44,6 +44,11 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+
+    # Other apps
+
+    # My apps
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -61,7 +66,10 @@ ROOT_URLCONF = 'toptech.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
