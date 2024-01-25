f0b4145c7eb637069e9ad9ac22ad7791eff4e929
b1ndark <>
2024-01-25 02:05:04 +0000

    add product models, image and migrate

--- products/models.py
+++ products/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+"""
+List of operating systems
+"""
+OS = (
+    ('android', 'Android'),
+    ('ios', 'iOS'),
+    ('linux', 'Linux'),
+    ('windows', 'Windows'),
+    ('macos', 'macOS'),
+)
+
+"""
+List of Makers/Brands
+"""
+BRANDS = (
+    ('acer', 'Acer'),
+    ('apple', 'Apple'),
+    ('asus', 'Asus'),
+    ('google', 'Google'),
+    ('toshiba', 'Toshiba'),
+    ('sony', 'Sony'),
+)
+
+
+class Category(models.Model):
+    """ Products category """
+    name = models.CharField(max_length=200)
+    friendly_name = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(
+        null=True, blank=True, upload_to='products/',
+        default='products/no_product_image.webp')
+    description = models.TextField()
+    brand = models.CharField(
+        max_length=30, choices=BRANDS, null=True, blank=True, default='')
+    operating_system = models.CharField(
+        max_length=30, choices=OS, null=True, blank=True, default='')
+    sku = models.CharField(max_length=200, null=True, blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
