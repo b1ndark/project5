e892982c159919765699bd764192d015daf5351c
b1ndark <>
2024-01-26 19:29:53 +0000

    add search functionality, fix sm-screens and choice names display

--- products/views.py
+++ products/views.py
@@ -1,21 +1,42 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
 def products(request):
-    """ This will render the All Products, will also render all the search and sorting queries """
+    """ This will render the All Products,
+    will also render all the search and sorting queries """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                message.error(
+                    request, "Please enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query) | Q(
+                    operating_system__icontains=query) | Q(
+                        brand__icontains=query) | Q(
+                            display__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
 
 
 def product_detail(request, product_id):
-    """ This will render the selected Product and display the information about it """
+    """ This will render the selected Product and
+    display the information about it """
 
     product = get_object_or_404(Product, pk=product_id)
 
