be0d2b40a6f7ebc50b5bce79fc6363d0188582ba
b1ndark <>
2024-01-26 02:04:38 +0000

    add product_detail template and functionality, update products template

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -12,3 +12,15 @@ def products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ This will render the selected Product and display the information about it """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
