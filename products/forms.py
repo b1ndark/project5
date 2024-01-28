ae8d9cb4dbff2c6b7c1dc2a6d7b97174d77d2ffe
b1ndark <>
2024-01-28 01:02:46 +0000

    add form for add_product

--- products/forms.py
+++ products/forms.py
@@ -2,3 +2,31 @@ from django import forms
 from .models import Product, category
 
 
+class ProductForm(forms.ModelForm):
+    """ Form to add a product to the store inventory """
+
+    class Meta:
+        model = Product
+        fields = (
+            'category',
+            'sku',
+            'name',
+            'brand',
+            'operating_system',
+            'memory_ram',
+            'display',
+            'battery_life',
+            'description',
+            'rating',
+            'price',
+            'image',
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init_(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded'
