057dedf081340644f596846b9f3bfa12f0358e2a
b1ndark <>
2024-01-28 18:49:26 +0000

    add delete_product template, view, urls and style

--- products/views.py
+++ products/views.py
@@ -147,3 +147,30 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def delete_product(request, product_id):
+    """ Delete a product from the store inventory
+        Will render a delete template to check if
+        you want to delete the selected product
+        Also check if User is Superuser
+    """
+
+    if not request.user.is_superuser:
+        messages.error(
+            request, 'You are not authorised to access Admin areas!')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        product.delete()
+        messages.success(request, 'Product has been deleted!')
+        return redirect(reverse('products'))
+
+    template = 'products/delete_product.html'
+    context = {
+        'product': product,
+    }
+
+    return render(request, template, context)
