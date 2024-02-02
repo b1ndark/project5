36afea82b52f7347b94d0f10131f95e37e846344
Vitor De Oliveira <vitor_oliveira_20@hotmail.com>
2024-02-02 23:02:24 +0000

    add btn +/- and js code, fix bug in basket template

--- static/js/products.js
+++ static/js/products.js
@@ -1,4 +1,6 @@
-// Sorting function to filter products by price, rating and name
+/* 
+* Sorting function to filter products by price, rating and name 
+*/
 
 $('#sort-selector').change(function() {
     var selector = $(this)
@@ -21,9 +23,57 @@ $('#sort-selector').change(function() {
     }
 })
 
-// To display which image has been selected before adding/updating product
+/*
+* To display which image has been selected before adding/updating product
+*/
 
 $('#new-image').change(function() {
     var file = $('#new-image')[0].files[0];
     $('#filename').text(`Image will be set to: ${file.name}`);
+});
+
+/*
+* To Increment and Decrement quantities, range from 1 to 20
+*/
+
+function handleEnableDisable(itemId) {
+    var currentValue = parseInt($(`#id_qty_${itemId}`).val());
+    var minusDisabled = currentValue < 2;
+    var plusDisabled = currentValue > 20;
+    $(`#decrement-qty_${itemId}`).prop('disabled', minusDisabled);
+    $(`#increment-qty_${itemId}`).prop('disabled', plusDisabled);
+}
+
+// To Check and enabling/disabling all inputs when page is loaded
+var allQtyInputs = $('.qty_input');
+for(var i = 0; i < allQtyInputs.length; i++) {
+    var itemId = $(allQtyInputs[i]).data('item_id');
+    handleEnableDisable(itemId);
+}
+
+
+// Check enable/disable every time the input is changed
+$('.qty_input').change(function() {
+    var itemId = $(this).data('item_id');
+    handleEnableDisable(itemId);
+});
+
+// To increment the quantity
+$('.increment-qty').click(function(e) {
+    e.preventDefault();
+    var closestInput =$(this).closest('.input-group').find('.qty_input')[0];
+    var currentValue = parseInt($(closestInput).val());
+    $(closestInput).val(currentValue + 1);
+    var itemId = $(this).data('item_id');
+    handleEnableDisable(itemId);
+});
+
+// To decrement the quantity
+$('.decrement-qty').click(function(e) {
+    e.preventDefault();
+    var closestInput =$(this).closest('.input-group').find('.qty_input')[0];
+    var currentValue = parseInt($(closestInput).val());
+    $(closestInput).val(currentValue - 1);
+    var itemId = $(this).data('item_id');
+    handleEnableDisable(itemId);
 });
--- toptech/settings.py
+++ toptech/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['8000-b1ndark-project5-7azh8olr5h.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-b1ndark-project5-muacqj8xxyx.ws-eu107.gitpod.io']
 
 
 # Application definition
