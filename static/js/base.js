e3d8fa80ffdee2773355606ebcbdcf967c025dcb
Vitor De Oliveira <vitor_oliveira_20@hotmail.com>
2024-02-06 00:54:24 +0000

    add animations to icons when hover

--- static/js/base.js
+++ static/js/base.js
@@ -3,3 +3,13 @@
 $('.carousel').carousel({
     interval: 4000
 });
+
+// Animations to the fontawesome icons when hover
+
+$('.fa-trash-can').hover(function() {
+    $(this).toggleClass('fa-shake');
+});
+
+$('.fa-minus, .fa-plus').hover(function() {
+    $(this).toggleClass('fa-beat');
+});
