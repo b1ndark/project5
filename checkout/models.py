dcc0176ab514ee2d03e699717a8cd1d3f478c259
Vitor De Oliveira <vitor_oliveira_20@hotmail.com>
2024-02-14 22:29:40 +0000

    add profile models, urls, views, default pic and template. migrate

--- checkout/models.py
+++ checkout/models.py
@@ -4,6 +4,7 @@ from django.db.models import Sum
 from django.conf import settings
 from django_countries.fields import CountryField
 from products.models import Product
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
@@ -11,6 +12,9 @@ class Order(models.Model):
 
     order_number = models.CharField(
         max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(
+        UserProfile, on_delete=models.SET_NULL, null=True, 
+        blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=200, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -38,7 +42,6 @@ class Order(models.Model):
         """
 
         return uuid.uuid4().hex.upper()
-
 
     def update_total(self):
         """
