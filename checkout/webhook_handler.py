9559cf81964bfbca1272ee0c6cdf01c5cdef8024
Vitor De Oliveira <vitor_oliveira_20@hotmail.com>
2024-03-08 14:47:41 +0000

    update order confirmation email and input arrows fix

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -28,7 +28,7 @@ class StripeWH_Handler:
             {'order': order}
         )
         body = render_to_string(
-            'checkout/confirmation_emails/confirmation_email_body.html',
+            'checkout/confirmation_emails/confirmation_email_body.txt',
             {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL}
         )
         send_mail(
