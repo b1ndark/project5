fbe15190de7ed82c944e81695d994778552f1a7e
Vitor De Oliveira <vitor_oliveira_20@hotmail.com>
2025-05-21 19:17:59 +0000

    new allauth and database

--- toptech/settings.py
+++ toptech/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['8000-b1ndark-project5-muacqj8xxyx.ws-eu110.gitpod.io', 'toptech-244e7b312287.herokuapp.com']
+ALLOWED_HOSTS = ['8000-b1ndark-project5-yrqhvba8yj4.ws-eu118.gitpod.io', 'toptech-244e7b312287.herokuapp.com']
 
 # Application definition
 
